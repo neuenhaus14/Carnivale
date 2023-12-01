de1b046359a0b35969a3e03f72555b80c03fc734
jshep23prog <jshep23@gmail.com>
2023-12-01 12:23:21 -0600

    "Initial setup and local deployment"

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config()
+
+const app = express();
+const port = 4000;
+
+const distPath = path.resolve(__dirname, '..', 'dist');
+
+app.use(express.static(distPath)); 
+
+// SERVER CONNECTION
+app.listen(port, () => {
+  console.log(`
+  Listening at: http://127.0.0.1:${port}
+  `);
+});
