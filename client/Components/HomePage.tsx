a1402cdfeaf18f63d5946a2f1879bf35efd83641
gfish94 <grmyfisher@gmail.com>
2023-12-04 17:33:34 -0600

    working on weather api call

--- client/Components/HomePage.tsx
+++ client/Components/HomePage.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {
+  Card,
+} from "react-bootstrap";
+import axios from 'axios';
+import { WEATHER_API_KEY } from '../../server/config';
 
 const HomePage = () => {
+
+  //location = lat,long
+  const location = '51.52, -0.11'
+  
+  const getWeather = ()=> {
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${location}&aqi=no`)
+      .then((data)=>{
+        console.log(data);
+      })
+      .catch((err) => console.error(err))
+  }
+
+  useEffect(getWeather, [])
 
 return (
   <div>
     <h1>HomePage!</h1>
+    <Card>
+
+    </Card>
   </div>
 )
 
