13516fad09a4046395a50e4110485a961276c534
neuenhaus14 <neuenhaus14@gmail.com>
2023-12-04 12:29:40 -0600

    merge with alex

--- server/index.ts
+++ server/index.ts
@@ -64,3 +64,42 @@ app.listen(port, () => {
   Listening at: http://127.0.0.1:${port}
   `);
 });
+// const express = require('express');
+// const path = require('path');
+// const { db } = require('./db');
+//require('dotenv').config()
+
+import express from 'express'
+import path from 'path'
+import {db} from './db'
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+// this inits the type for req/res for typescript
+import { Request, Response } from 'express';
+
+const app = express();
+const port = 4000;
+
+const routeHandler = express.Router()
+const distPath = path.resolve(__dirname, '..', 'dist');
+
+app.use(express.static(distPath)); 
+app.use('/', routeHandler)
+
+
+app.get('/*', function(req: Request, res: Response) {
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
+// SERVER CONNECTION
+app.listen(port, () => {
+  console.log(`
+  Listening at: http://127.0.0.1:${port}
+  `);
+});
---
+++ webpack.config.ts
@@ -0,0 +1,108 @@
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+//const webpack = require('webpack');
+//const WebpackBar = require('webpackbar');
+import WebpackBar from 'webpackbar'
+//const path = require('path');
+import path from 'path'
+import { fileURLToPath } from 'url';
+//const Dotenv = require('dotenv-webpack');
+import Dotenv from 'dotenv-webpack'
+//const autoprefixer = require('autoprefixer');
+import autoprefixer from 'autoprefixer'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const SRC_DIR = path.resolve(__dirname, 'client');
+const DIST_DIR = path.resolve(__dirname, 'dist');
+
+module.exports = {
+
+  entry: {
+    app: path.resolve(SRC_DIR, 'index.tsx'),
+  },
+
+  output: {
+    path: DIST_DIR,
+    filename: '[name].bundle.js',
+  },
+  mode: "none",
+  devtool: 'inline-source-map',
+
+  plugins: [
+    // Creates a loading bar
+    new WebpackBar(),
+    // Clears files in ./dist
+    // new CleanWebpackPlugin(),
+    // generates an html file from template
+    new HtmlWebpackPlugin({
+      template: path.resolve(SRC_DIR, 'index.html'),
+    }),
+
+    new Dotenv()
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|jpg|png|mp3|aac|ogg)$/,
+        type: 'asset/resource',
+      },
+      { //for typescript
+        test: /\.tsx?$/, //match the least number of files it needs to 
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(js|jsx)$/, //look through files if end in js or jsx run with babel loader
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-react', { runtime: 'automatic' }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader',
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader',
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer,
+                ],
+              },
+            },
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  
+}
