885bd03b762fceaf714adb08aa7bd2e423c4c019
alexmbeasley <alexmbeasley@gmail.com>
2023-12-05 17:54:14 -0600

    Feed page shows shared post

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, Router } from "express";
 import path from "path";
-import { db } from "./db";
+import { db, Join_shared_post, User } from "./db";
 import dotenv from "dotenv";
 
 //this is declaring db as an obj so it can be ran when server starts
@@ -18,6 +18,30 @@ const distPath = path.resolve(__dirname, "..", "dist");
 app.use(express.static(distPath));
 app.use("/", routeHandler);
 
+app.get("/api/shared-posts/:user_id", async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.user_id;
+    const sharedPosts = await Join_shared_post.findAll({
+      where: { recipient_userId: userId },
+    });
+    res.json(sharedPosts);
+  } catch (error) {
+    console.error("Error getting shared posts:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/api/user/:user_id", async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.user_id;
+    const user = await User.findByPk(userId);
+    res.json(user);
+  } catch (error) {
+    console.error("Error getting user information:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 app.get("/*", function (req: Request, res: Response) {
   res.sendFile(
     path.join(__dirname, "..", "dist", "index.html"),
@@ -28,7 +52,6 @@ app.get("/*", function (req: Request, res: Response) {
     }
   );
 });
-
 // SERVER CONNECTION
 app.listen(port, () => {
   console.log(`
