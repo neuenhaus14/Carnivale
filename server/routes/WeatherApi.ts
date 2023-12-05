12811e9b84015acdc1d14c67950c22f485f58435
gfish94 <grmyfisher@gmail.com>
2023-12-05 17:31:10 -0600

    remove console log

--- server/routes/WeatherApi.ts
+++ server/routes/WeatherApi.ts
@@ -7,15 +7,12 @@ const WeatherRouter = express.Router();
 
 WeatherRouter.get('/:location', async (req: Request, res: Response) => {
   const { location } = req.params;
-  console.log(WEATHER_API_KEY)
-
-  // axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${location}&aqi=no`).then((data) => res.status(200).send(data)).catch((err) => res.sendStatus(500));
   try {
     const { data } = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${location}&aqi=no&alerts=no`);
     res.status(200)
       .send(data);
   } catch (err) {
-    //console.error(err);
+    console.error(err);
     res.sendStatus(500);
   }
 });
