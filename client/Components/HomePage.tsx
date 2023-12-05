51ab889e5e73f7a29ff5c94afc8b370887ac59d3
gfish94 <grmyfisher@gmail.com>
2023-12-05 16:09:50 -0600

    moved api call to server

--- client/Components/HomePage.tsx
+++ client/Components/HomePage.tsx
@@ -1,25 +1,10 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {
   Card,
 } from "react-bootstrap";
-import axios from 'axios';
-import  WEATHER_API_KEY  from '../../server/config'
+
 
 const HomePage = () => {
-  // //location = lat,long
-  const location = '51.52, -0.11'
-  
-  //TODO: typescript doesnt do string interpolation at runtime
-  const getWeather = ()=> {
-    axios.get('http://api.weatherapi.com/v1/forecast.json?key=' + WEATHER_API_KEY + '&q=' + location + '&aqi=no')
-      .then((data)=>{
-        console.log(data);
-      })
-      .catch((err) => console.error(err))
-  }
-
-  getWeather();
-
 
 return (
   <div>
