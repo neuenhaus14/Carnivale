067ea72bb2b6e35cfb7c3ad60cc053d176e37263
neuenhaus14 <neuenhaus14@gmail.com>
2023-12-06 11:06:05 -0600

    adds two options for CreatePin, neither work yet, but either through button or map click

--- client/Components/MapPage.tsx
+++ client/Components/MapPage.tsx
@@ -1,36 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import MapPageMap from './MapPageMap'
+import { Outlet, Link } from 'react-router-dom';
 import axios from 'axios';
 
 const MapPage = () => {
+  const [showOutlet, setShowOutlet] = useState(false);
+  const [newPin, setNewPin] = useState({
+    longitude: null,
+    latitude: null,
+    isToilet: false,
+    isFood: false,
+    isPersonal: false,
+    isFree: true,
+    ownerId: null,
+  });
 
-  // const getPins = () => {
-  //   axios.get('/pins/get-pins')
-  //   .then((response) => {
-  //     console.log(response.data)
-  //   })
-  //   .catch((err) => {
-  //     console.error(err)
-  //   })
-  // }
-
-
-  // useEffect({
-  //   axios.get('/pins/get-pins')
-  //   .then((response) => {
-  //     console.log(response.data)
-  //   })
-  //   .catch((err) => {
-  //     console.error(err)
-  //   })
-  // })
-
+  const buttonClick = () => {
+    setShowOutlet(true);
+  };
 
   return (
     <div>
       <h1>MapPage!</h1>
       <MapPageMap />
-      
+      <div id='create-pin'>
+        <p>Add a Pin Below!</p>
+        <div id="create-pin-buttons">
+          <Link to='/createpin'>
+            <button type="button" onClick={buttonClick}>Add Pin</button>
+          </Link>
+          </div>
+        </div> 
     </div>
   )
 
