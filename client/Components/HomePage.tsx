4244baa00ae1d68cc202a2a6c2d04f6c79c07b01
gfish94 <grmyfisher@gmail.com>
2023-12-06 17:18:45 -0600

    migrate WeatherCard to new component

--- client/Components/HomePage.tsx
+++ client/Components/HomePage.tsx
@@ -1,45 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { Card, Form, Button } from "react-bootstrap";
 import axios from "axios";
 
-const HomePage = () => {
-  const [location, setLocation] = useState("");
-  const [currWeather, setCurrWeather] = useState("");
-  const [currTemp, setCurrTemp] = useState("");
-  const [avgTemp, setAvgTemp] = useState("");
+import WeatherCard from "./WeatherCard";
+import PostCard from "./PostCard";
 
-  const getWeather = async () => {
+const HomePage = () => {
+  const [comment, setComment] = useState("");
+  const [userId, setUserId] = useState('1');
+  const [posts, setPosts] = useState(null);
+
+  const handleSubmit = async() => {
     try {
-      const { data } = await axios.get(`/api/weather/${location}`);
-      setCurrWeather(data.current.condition.text);
-      setCurrTemp(data.current.temp_f);
-      setAvgTemp(data.forecast.forecastday[0].day.avgtemp_f);
-      console.log(data);
-    } catch (err) {
+      axios.post(`/api/home/${userId}`, { comment });
+    } catch (err){
+      console.error(err);
+    } finally {
+      getPosts();
+    }
+  }
+
+  const getPosts = async () => {
+    try {
+      const { data } = await axios.get(`/api/home/posts`);
+      setPosts(data);
+    } catch (err){
       console.error(err);
     }
-  };
+  }
+
+  useEffect(() => {
+    getPosts();
+    const interval = setInterval(() => {
+      getPosts();
+      console.log('fetch');
+    }, 5000);
+    return () => clearInterval(interval)
+  }, [])
+
 
   return (
     <div>
       <h1>HomePage!</h1>
-      <Card>
-        {
-        `Current Weather: ${currWeather},
-        Current Temp: ${currTemp},
-        Avg Temp: ${avgTemp}
-        `
-      }
-      </Card>
+
+      {/* temporary */}
       <Form>
         <Form.Group>
-          <Form.Label>Location</Form.Label>
-          <Form.Control onChange={(e) => setLocation(e.target.value)} />
+          <Form.Label>UserId</Form.Label>
+          <Form.Control onChange={(e) => setUserId(e.target.value)} />
+        </Form.Group>
+      </Form>
+      {/* temporary */}
+
+      <WeatherCard />
+      {
+        posts
+        ? posts.map((item: any, index: number) => (
+          <PostCard key={`${item.id} + ${index}`} post={item}/>
+        ))
+        : ''
+      }
+      <Form>
+        <Form.Group>
+          <Form.Label>COMMENT</Form.Label>
+          <Form.Control onChange={(e) => setComment(e.target.value)} />
           <Button
             variant="primary"
-            onClick={() => getWeather()}
+            onClick={() => handleSubmit()}
           >
-            GET WEATHER!!!
+            SEND!!!
           </Button>
         </Form.Group>
       </Form>
