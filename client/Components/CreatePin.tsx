ee96b915d261642c565c68a2c8a487379a2df4cf
neuenhaus14 <neuenhaus14@gmail.com>
2023-12-06 13:11:43 -0600

    modal appears on click but cannot repeat process

--- client/Components/CreatePin.tsx
+++ client/Components/CreatePin.tsx
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
+import { Modal, Button } from 'react-bootstrap'
 
-const CreatePin = () => {
- 
+const CreatePin = ({isShow}) => {
+  const [isShown, setShow] = useState(isShow);
+
+  const initModal = () => {
+    setShow(!isShown); 
+    console.log(isShown)
+   
+  };
+
+  console.log(isShown)
 
   return (
-    <div>
-      <h1>CreatePin!</h1>
-  
-    </div>
-)
-
-};
-
-// interface FormProps {
-//   onSubmit: (data: FormData) => void;
-// }
-
-// interface FormData {
-//   longitude: number;
-//   latitude: number;
-//   isToilet: boolean;
-//   isFood: boolean;
-//   isPersonal: boolean;
-//   isFree: boolean;
-//   ownerId: number;
-// }
+    <>
+      {/* <Button variant="success" onClick={initModal}>
+        Open Modal
+      </Button> */}
+      <Modal show={isShown} onHide={initModal}>
+        <Modal.Header closeButton >
+          <Modal.Title>React Modal Popover Example</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={initModal}>
+            Close
+          </Button>
+          <Button variant="dark" onClick={initModal}>
+            Store
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
 
 export default CreatePin;
