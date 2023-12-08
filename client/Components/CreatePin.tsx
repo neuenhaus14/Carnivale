55f9a03a6fe00bcb2e1a650152197689e8d07698
neuenhaus14 <neuenhaus14@gmail.com>
2023-12-07 19:06:24 -0600

    tyring to rtore

---
+++ client/Components/CreatePin.tsx
@@ -0,0 +1,76 @@
+import React, {useState, useEffect} from 'react'
+import { Modal, Button, Form } from 'react-bootstrap'
+import { useParams} from 'react-router-dom'
+ 
+const CreatePin = ( change: any, searchParams: any ) => {
+   const [isShow, setShow] = useState(true);
+   const { lng } = useParams()
+   const { lat } = useParams()
+
+  //  console.log(lng, lat)
+  //  console.log(searchParams)
+
+    const urlSearchString = window.location.search;
+    const params = new URLSearchParams(urlSearchString);
+    console.log(params)
+   
+    
+
+  const initModal = () => {
+    setShow(!isShow); 
+    change(!isShow)
+    console.log(isShow)
+  };
+
+  const saveCreatedPin = (e: any) => {
+    const selectedCategory = e.target.value // "isToilet"
+    console.log(e.target)
+    console.log(e.target.value)
+  }
+
+  return (
+    <>
+      {/* <Button variant="success" onClick={initModal}>
+        Open Modal
+      </Button> */}
+      <Modal show={isShow} onHide={initModal}>
+        <Modal.Header closeButton >
+          <Modal.Title>Create a Pin</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Select a category, add a picture, and save your pin. 
+          <Form>
+            <Form.Group className ='mb-3' controlId="pinType">
+              <Form.Check type="radio" id="freeToilet" name="pin-cat" 
+                label="Free Toilet" value="isFree" inline isValid 
+                onClick={(e) => {saveCreatedPin(e)} }
+                />
+              <Form.Check type="radio" id="toilet" name="pin-cat" 
+                label="Toilet" value="isToilet" inline isValid 
+                onClick={(e) => {saveCreatedPin(e)} }
+                />
+              <Form.Check type="radio" id="food" name="pin-cat" 
+                label="Food" value="isFood" inline isValid 
+                onClick={(e) => {saveCreatedPin(e)} }
+                />
+              <Form.Check type="radio" id="personal" name="pin-cat" 
+                label="Personal" value="isPersonal" inline isValid 
+                onClick={(e) => {saveCreatedPin(e)} }
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="picture spot" >
+              <Form.Label>Placeholder for Picture</Form.Label>
+              <Form.Control as="textarea" rows={5} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={initModal}> Close </Button>
+          <Button variant="dark" onClick={initModal}> Save </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+export default CreatePin;
---
+++ client/Components/MapPageMap.tsx
@@ -0,0 +1,103 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useRef, useState, useContext } from "react";
+import { Map, Marker, NavigationControl, Layer, Source } from 'react-map-gl';
+import { BsFillPinFill } from "react-icons/bs";
+import { Link, useSearchParams } from 'react-router-dom';
+import axios from 'axios';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import CreatePin from './CreatePin';
+
+// type DroppedPin = {
+//     lng: number,
+//     lat: number,
+// }
+
+const MapPageMap = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [createPin, setCreatePin] = useState(false);
+  const [markers, setMarkers] = useState([]);
+  const [droppedPin, setDroppedPin] = useState({
+    lng: null,
+    lat: null
+  })
+  const [viewState, setViewState] = useState({
+    latitude: 29.964735,
+    longitude: -90.054261,
+    zoom: 14,
+  });
+
+  //loads pins immediately on page render, once
+  useEffect(() => {
+    getPins();
+  }, []);
+
+  const getPins = async () => {
+    try {
+      const response = await axios.get('/api/pins/get-pins')
+      setMarkers(response.data)
+    } catch (err)  {
+      console.error(err)
+    }
+  }
+
+  const dropPin = (e: any) => {
+    console.log(e.lngLat.lng, e.lngLat.lat)
+    setDroppedPin({
+      lng: e.lngLat.lng,
+      lat: e.lngLat.lat})
+
+    createPinState()
+    setSearchParams({lng:`${e.lngLat.lng}` , lat:`${e.lngLat.lat}`})  
+
+  }
+
+
+  const createPinState = () => {
+    setCreatePin(!createPin)
+  }
+
+  const clickedMarker = (e: any) => {
+    const currMarkerLng = e._lngLat.lng;
+    const currMarkerLat = e._lngLat.lat;
+
+    console.log(e._lngLat.lng, e._lngLat.lat);
+
+  };
+
+  const mapRef = useRef(null);
+
+  return (
+    <div>
+      { createPin ? <CreatePin change={createPinState} searchParams={searchParams} /> : null }
+      <div id='map-page-filter' >
+      </div>
+      <Map
+        ref={mapRef}
+        {...viewState}
+        onMove={(e) => setViewState(e.viewState)}
+        onClick={(e) => {dropPin(e)}}
+        mapboxAccessToken="pk.eyJ1IjoiZXZtYXBlcnJ5IiwiYSI6ImNsb3hkaDFmZTBjeHgycXBpNTkzdWdzOXkifQ.BawBATEi0mOBIdI6TknOIw"
+        style={{ position: 'relative', bottom: '0px', width: '100vw', height: 475 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+      <div id='map-markers'>
+        {
+          markers.map((marker) => (
+            <Marker 
+            key={marker.id}
+            onClick={(e) => clickedMarker(e.target)} 
+            longitude={marker.longitude} latitude={marker.latitude}
+            anchor="bottom"> 
+            <BsFillPinFill style={{ width: 25, height: 50}} /> 
+            </Marker>
+          ))
+        }
+      </div>
+      <NavigationControl />
+      </Map>
+    </div>
+  )
+}
+
+export default MapPageMap;
