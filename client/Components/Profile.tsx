7c4c39c7848a0a36ad450c7124dfcaf459eda975
gfish94 <grmyfisher@gmail.com>
2023-12-08 16:43:31 -0600

    auth 0 testing

--- client/Components/Profile.tsx
+++ client/Components/Profile.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -19,4 +20,4 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+export default withAuthenticationRequired(Profile);
