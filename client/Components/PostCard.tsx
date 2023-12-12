5101590752a07235b7c3a32a6a7bae9477067bd9
gfish94 <grmyfisher@gmail.com>
2023-12-11 18:25:04 -0600

    add owner to post

--- client/Components/PostCard.tsx
+++ client/Components/PostCard.tsx
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
 import { Card, Button } from "react-bootstrap";
 import dayjs from "dayjs";
-import relativeTime from 'dayjs/plugin/relativeTime'
+import relativeTime from "dayjs/plugin/relativeTime";
 import axios from "axios";
 
 dayjs.extend(relativeTime);
 
 const PostCard = (props: { post: any }) => {
   const { post } = props;
+  const [owner, setOwner] = useState("");
+
+  const getOwner = async () => {
+    try {
+      const { data } = await axios.get(`api/home/post/${post.ownerId}`);
+      //TODO: change to name
+      setOwner(data.email);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  getOwner();
+
   return (
     <div>
       {post.comment ? (
         <Card>
           <Card.Text>
-            {post.comment} - {dayjs(post.createdAt.toString()).fromNow()}
+            {post.comment} - {owner}:{" "}
+            {dayjs(post.createdAt.toString()).fromNow()}
           </Card.Text>
         </Card>
       ) : (
@@ -23,7 +37,8 @@ const PostCard = (props: { post: any }) => {
             src={post.photoURL}
           />
           <Card.Text>
-            {post.description} - {dayjs(post.createdAt.toString()).fromNow()}
+            {post.description} - {owner}:{" "}
+            {dayjs(post.createdAt.toString()).fromNow()}
           </Card.Text>
         </Card>
       )}
