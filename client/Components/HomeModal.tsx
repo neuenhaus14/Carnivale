9650f7fd5772e7e0a0919a265b8daafda38c9bcd
jshep23prog <jshep23@gmail.com>
2023-12-12 18:41:40 -0600

    "Added posting functionality from homepage, fixed logic photos side to make things work"

--- client/Components/HomeModal.tsx
+++ client/Components/HomeModal.tsx
@@ -6,65 +6,37 @@ import Photos from './Photos'
 //CHILD OF HOMEPAGE
 interface Props {
   setShowModal: any
+  lat: number
+  lng: number
 }
-//TODO: Gretchen will add a function to get lat long
 
 const HomeModal: React.FC<Props> = ( {
-  setShowModal
+  setShowModal, lat, lng
 } ) => {
   const [isShow, setIsShow] = useState(true);
   const [isThrow, setIsThrow] = useState(false);
   const [isCostume, setIsCostume] = useState(false);
 
-  const { lngPost }  = -90;
-  const {latPost }  = 30;
-  // const  lngRounded = Math.round(lng * 10000) / 10000;
-  // const  latRounded = Math.round(lat * 10000) / 10000;
+  //func to reset boolean state if hot goss is clicked
   const resetBooleanState = () => {
     setIsThrow(false)
     setIsCostume(false)
   }
+
   //function for init modal
   const initModal = ()  => {
-    setIsShow(!isShow); 
+    setIsShow(!isShow);
     setShowModal(!isShow);
   }
 
   const createPhoto = async () => {
     initModal()
     try{
-      const { data } = await axios.post(`/api/home/create-post/${1}`, {
-        options: {
-          longitude: lngPost,
-          latitude: latPost,
-          isCostume,
-          isThrow
-        }
-      })
-      console.log('Post sent to database')
+     console.log('test')
     } catch (err)  {
       console.error(err)
     }
   }
-
-  // const savePhotoPost = async () => {
-  //   try{
-  //     const { data } = await axios.post(`/api/pins/create-pin/${1}`, {
-  //       options: {
-  //         longitude: lng,
-  //         latitude: lat,
-  //         isToilet,
-  //         isFood,
-  //         isPersonal,
-  //         isFree,
-  //       }
-  //     })
-  //     console.log('whish! pin sent to database')
-  //     setMarkers(markers.concat([data]))
-  //   } catch (err)  {
-  //     console.error(err)
-  //   }
-  // }
 
 
   return (
@@ -76,22 +48,27 @@ const HomeModal: React.FC<Props> = ( {
           Is this a costume? Mardi Gras Throw? Or Hot Goss?
           <Form>
             <Form.Group className ='mb-3' controlId="pinType">
-              <Form.Check type="radio" id="costume" name="pin-cat" 
-                label="Costume"  inline isValid 
+              <Form.Check type="radio" id="costume" name="pin-cat"
+                label="Costume"  inline isValid
                 onClick={() => {resetBooleanState(); setIsCostume(!isCostume)} }
                 />
-               <Form.Check type="radio" id="throw" name="pin-cat" 
-                label="Throw"  inline isValid 
+               <Form.Check type="radio" id="throw" name="pin-cat"
+                label="Throw"  inline isValid
                 onClick={() => {resetBooleanState(); setIsThrow(!isThrow)} }
-                />  
-                <Form.Check type="radio" id="goss" name="pin-cat" 
+                />
+                <Form.Check type="radio" id="goss" name="pin-cat"
                 label="Goss" value="goss" inline isValid 
                 onClick={() => {resetBooleanState()} }
                 />
             </Form.Group>
             <Form.Group className="mb-3" controlId="picture spot" >
-              <Form.Label>Add a picture below!</Form.Label>
-              <Photos latPost={latPost} lngPost={lngPost} createPhoto={createPhoto}/>
+              <Photos
+              latPost={lat}
+              lngPost={lng}
+              isThrow={isThrow}
+              isCostume={isCostume}
+              createPhoto={createPhoto}
+              />
             </Form.Group>
           </Form>
         </Modal.Body>
