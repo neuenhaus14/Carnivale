267458b71a614f1de4a798880972ac9504dff238
gfish94 <grmyfisher@gmail.com>
2023-12-14 14:04:52 -0600

    share modal requires postId: num, userId: num, and postType: string

--- client/Components/PostCard.tsx
+++ client/Components/PostCard.tsx
@@ -65,7 +65,7 @@ const PostCard = (props: { post: any; userId: number }) => {
             {dayjs(post.createdAt.toString()).fromNow()}
           </Card.Text>
           <ShareModal
-            post={post}
+            post={post.id}
             userId={userId}
             postType={"comment"}
           />
@@ -84,7 +84,7 @@ const PostCard = (props: { post: any; userId: number }) => {
             {dayjs(post.createdAt.toString()).fromNow()}
           </Card.Text>
           <ShareModal
-            post={post}
+            post={post.id}
             userId={userId}
             postType={"photo"}
           />
