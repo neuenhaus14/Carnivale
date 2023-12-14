b0aaa484252cb3bd87eab7f99085bec5d26a984e
alexmbeasley <alexmbeasley@gmail.com>
2023-12-14 08:20:30 -0600

    sites being scrapped

--- server/index.ts
+++ server/index.ts
@@ -8,13 +8,14 @@ import PinRoutes from './routes/Pins';
 import http from 'http'
 import cors from 'cors'
 //import Upload  from "./routes/PhotoUpload"
-import cloudinary  from "./utils/cloudinary_helpers" //grabbing reference to an already configured cloudinary object
-import FriendsRoutes from './routes/Friends'
+import cloudinary from "./utils/cloudinary_helpers"; //grabbing reference to an already configured cloudinary object
+import FriendsRoutes from "./routes/Friends";
 import WeatherRoutes from "./routes/WeatherApi";
-import EventsRoutes from './routes/Events'
+import EventsRoutes from "./routes/Events";
 import HomeRoutes from "./routes/Home";
 import FeedRoutes from "./routes/Feed";
-import ImageRouter from './routes/PhotoUpload'
+import ImageRouter from "./routes/PhotoUpload";
+import ParadesRoutes from "./routes/Parades";
 
 import { User } from './db/index'
 
@@ -34,14 +35,15 @@ const distPath = path.resolve(__dirname, "..", "dist");
 app.use(express.static(distPath));
 app.use(express.json());
 
-app.use('/api/home', HomeRoutes)
-app.use('/api/friends', FriendsRoutes)
+app.use("/api/home", HomeRoutes);
+app.use("/api/friends", FriendsRoutes);
 app.use("/api/events", EventsRoutes);
-app.use('/api/weather', WeatherRoutes)
+app.use("/api/weather", WeatherRoutes);
 app.use("/", routeHandler);
 app.use("/api/pins", PinRoutes);
 app.use("/api/feed", FeedRoutes);
-app.use('/api/images', ImageRouter)
+app.use("/api/images", ImageRouter);
+app.use("/api/parades", ParadesRoutes);
 app.use(cors({
   origin: ['http://localhost:4000'], 
   credentials: true
@@ -51,18 +53,38 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: AUTH0_CLIENT_SECRET,
-  baseURL: 'http://localhost:4000',
+  baseURL: "http://localhost:4000",
   clientID: AUTH0_CLIENT_ID,
-  issuerBaseURL: ISSUER
+  issuerBaseURL: ISSUER,
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
 // req.isAuthenticated is provided from the auth router
-app.get('/auth', (req, res) => {
+app.get("/auth", (req, res) => {
   console.log(req.oidc.isAuthenticated());
-  res.render("index", {isAuthenticated: req.oidc.isAuthenticated()});
+  res.render("index", { isAuthenticated: req.oidc.isAuthenticated() });
+});
+
+io.on('connection', (socket: any) => {
+  console.log('a user connected');
+  socket.on('userLoc', (userLoc: any) => {
+    console.log('userLoc', userLoc.longitude, userLoc.latitude, userLoc.id)
+      User.update({longitude: userLoc.longitude, latitude: userLoc.latitude}, {where: {id: userLoc.id}})
+      .then(() => 
+        User.findOne({where: {id: userLoc.id}})
+          .then((data) => {
+            console.log('successfully updated location')
+            io.emit('userLoc', data.dataValues)
+          })
+      )
+      .catch((err) => console.error(err))
+  })
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+  });
 });
 
 io.on('connection', (socket: any) => {
@@ -105,7 +127,5 @@ server.listen(port, () => {
 });
 
 // this inits the type for req/res for typescript
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
-
-
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
