7b82c5b31fd0ec5b2a99a596bc68a2dfac32c76b
evmaperry <evmaperry@gmail.com>
2023-12-15 13:31:52 -0500

    CLIENT n SERVER / Add link to eventpage for public events, add routes to get public events user is invited to and attending

--- client/Components/EventPage.tsx
+++ client/Components/EventPage.tsx
@@ -1,16 +1,73 @@
-import React from 'react';
-import EventAttendingMapComponent from './EventBasicMapComponent';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import EventBasicModal from './EventBasicModal';
+import axios from 'axios';
 
 
-const EventPage = () => {
+interface EventPageProps {
+  getLocation: any,
+  lng: number,
+  lat: number,
+}
+
+const EventPage: React.FC<EventPageProps> = ({getLocation, lng, lat}) => {
+  
+  const [searchParams] = useSearchParams();
+  const [userId] = useState(Number(searchParams.get('userid')) || 1);
+  const [friends, setFriends] = useState([]);
+  
+  const [selectedEvent, setSelectedEvent] = useState({ latitude: 0, longitude: 0, startTime: null, endTime: null })
+  const [isUserAttending, setIsUserAttending] = useState(false)
+  
+  const [eventsParticipating, setEventsParticipating] = useState([{ name: 'event1' }]);
+  const [eventsInvited, setEventsInvited] = useState([{ name: 'event2' }])
+  
+  const [showBasicModal, setShowBasicModal] = useState(false)
+
+  // specifically for public events
+  const getEventsInvited = async () => {
+    const publicEventsInvited = await axios.get(`/api/events/getPublicEventsParticipating/${userId}`)
+    setEventsInvited(publicEventsInvited.data)
+  }
+
+  // specifically for public events
+  const getEventsParticipating = async () => {
+    const publicEventsParticipating = await axios.get(`/api/events/getPublicEventsInvited/${userId}`)
+    setEventsParticipating(publicEventsParticipating.data);
+  }
+
+
+
+  // location
+  useEffect(() => {
+    getLocation()
+    getEventsInvited()
+    getEventsParticipating()
+  }, [])
+
+
+
+
 
   return (
     <div>
-      <h1>EventPage!</h1>
-      {/* <EventAttendingMapComponent /> */}
+      <h1>EventPage! {`${lng}, ${lat}`}</h1>
+
+      <EventBasicModal
+        selectedEvent={selectedEvent}
+        setSelectedEvent={setSelectedEvent}
+        setShowBasicModal={setShowBasicModal}
+        showBasicModal={showBasicModal}
+        friends={friends}
+        userId={userId}
+        isUserAttending={isUserAttending}
+        setIsUserAttending={setIsUserAttending}
+        getEventsInvited={getEventsInvited}
+        getEventsParticipating={getEventsParticipating}
+      />
     </div>
-  
-)
+
+  )
 
 };
 
