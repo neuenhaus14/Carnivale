ef5cc9106f0f2b9dec08720f40296683cff33527
evmaperry <evmaperry@gmail.com>
2023-12-29 16:15:25 -0500

    CLIENT / Styling basic modal

--- client/Components/EventBasicModal.tsx
+++ client/Components/EventBasicModal.tsx
@@ -4,7 +4,11 @@ import EventBasicMapComponent from './EventBasicMapComponent';
 import axios from 'axios';
 import dayjs = require('dayjs');
 import relativeTime from 'dayjs/plugin/relativeTime';
+import calendar from 'dayjs/plugin/calendar';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+dayjs.extend(customParseFormat);
 dayjs.extend(relativeTime);
+dayjs.extend(calendar);
 
 // This modal displays for events that the user
 // is invited to and attending
@@ -23,253 +27,6 @@ interface EventBasicModalProps {
   getEventsInvited: any;
   getEventsParticipating: any;
 }
-
-interface EventBasicAccordionProps {
-  friends: any;
-  selectedEvent: any;
-  userId: number;
-  isUserAttending: boolean;
-}
-
-interface EventBasicKreweTabProps {
-  friends: any;
-  selectedEvent: any;
-  userId: number;
-  isUserAttending: boolean;
-}
-
-// MAYBE ADD A useEffect THAT FETCHES WHO'S ATTENDING
-// AN EVENT WHENEVER SELECTED EVENT CHANGES
-
-const EventBasicAccordion: React.FC<EventBasicAccordionProps> = ({
-  friends,
-  selectedEvent,
-  userId,
-  isUserAttending,
-}) => {
-  const [invitees, setInvitees] = useState([]);
-  const [participants, setParticipants] = useState([]);
-  const [friendsToInvite, setFriendsToInvite] = useState([]); // collects friends to invite as group to event
-
-  useEffect(() => {
-    getPeopleForEvent();
-  }, []);
-
-  // ONLY A USER'S FRIENDS WILL POPULATE THESE AREA
-  const getPeopleForEvent = async () => {
-    const eventPeopleData = await axios.get(
-      `/api/events/getPeopleForEvent/${userId}-${selectedEvent.id}`
-    );
-    const { eventParticipants, eventInvitees } = eventPeopleData.data;
-    // console.log('inside getPeopleForEvent', 'eP', eventParticipants, 'eI', eventInvitees)
-    setInvitees(eventInvitees);
-    setParticipants(eventParticipants);
-  };
-
-  const toggleFriendInvite = (invitee_userId: number) => {
-    // if the user is already being invited
-    if (friendsToInvite.includes(invitee_userId)) {
-      // remove them
-      setFriendsToInvite(
-        friendsToInvite.filter((friend) => friend !== invitee_userId)
-      );
-    } else {
-      // or else add them to invite list
-      setFriendsToInvite([...friendsToInvite, invitee_userId]);
-    }
-  };
-
-  const sendFriendInvites = async () => {
-    try {
-      const inviteResponse = await axios.post('/api/events/inviteToEvent', {
-        invitations: {
-          eventId: selectedEvent.id,
-          invitees: friendsToInvite,
-        },
-      });
-      // console.log('iR', inviteResponse)
-      getPeopleForEvent();
-      setFriendsToInvite([]);
-    } catch (err) {
-      console.error('CLIENT ERROR: failed to POST event invites', err);
-    }
-  };
-
-  const attendingFriendsItems = friends
-    .filter((friend: any) => participants.includes(friend.id))
-    .map((friend: any, index: number) => {
-      return (
-        <li key={index}>
-          {friend.firstName} {friend.lastName}: attending
-        </li>
-      );
-    });
-
-  const invitedFriendsItems = friends
-    .filter((friend: any) => invitees.includes(friend.id))
-    .map((friend: any, index: number) => {
-      return (
-        <li key={index}>
-          {friend.firstName} {friend.lastName}: invited
-        </li>
-      );
-    });
-
-  const uninvitedFriendsItems = friends
-    .filter(
-      (friend: any) =>
-        !participants.includes(friend.id) && !invitees.includes(friend.id)
-    )
-    .map((friend: any, index: number) => {
-      return (
-        <li key={index}>
-          {friend.firstName} {friend.lastName}: not invited
-          {isUserAttending && (
-            <Form.Check
-              type='checkbox'
-              id='invite-checkbox'
-              label='Check to queue invite'
-              onChange={() => toggleFriendInvite(friend.id)}
-            />
-          )}
-        </li>
-      );
-    });
-
-  // console.log('inside event modal accordion. i:', invitees, 'p', participants, 'f2I', friendsToInvite);
-  return (
-    <Accordion>
-      <Accordion.Item eventKey='0'>
-        <Accordion.Header>Who's Going?</Accordion.Header>
-        <Accordion.Body>
-          <ul>{attendingFriendsItems}</ul>
-          <ul>{invitedFriendsItems}</ul>
-          <ul>{uninvitedFriendsItems}</ul>
-          {isUserAttending && (
-            <Button
-              disabled={friendsToInvite.length === 0}
-              onClick={() => sendFriendInvites()}
-            >
-              Send Invites
-            </Button>
-          )}
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-  );
-};
-
-const EventBasicKreweTab: React.FC<EventBasicKreweTabProps> = ({
-  friends,
-  selectedEvent,
-  userId,
-  isUserAttending,
-}) => {
-  const [invitees, setInvitees] = useState([]);
-  const [participants, setParticipants] = useState([]);
-  const [friendsToInvite, setFriendsToInvite] = useState([]); // collects friends to invite as group to event
-
-  useEffect(() => {
-    getPeopleForEvent();
-  }, []);
-
-  // ONLY A USER'S FRIENDS WILL POPULATE THESE AREA
-  const getPeopleForEvent = async () => {
-    const eventPeopleData = await axios.get(
-      `/api/events/getPeopleForEvent/${userId}-${selectedEvent.id}`
-    );
-    const { eventParticipants, eventInvitees } = eventPeopleData.data;
-    // console.log('inside getPeopleForEvent', 'eP', eventParticipants, 'eI', eventInvitees)
-    setInvitees(eventInvitees);
-    setParticipants(eventParticipants);
-  };
-
-  const toggleFriendInvite = (invitee_userId: number) => {
-    // if the user is already being invited
-    if (friendsToInvite.includes(invitee_userId)) {
-      // remove them
-      setFriendsToInvite(
-        friendsToInvite.filter((friend) => friend !== invitee_userId)
-      );
-    } else {
-      // or else add them to invite list
-      setFriendsToInvite([...friendsToInvite, invitee_userId]);
-    }
-  };
-
-  const sendFriendInvites = async () => {
-    try {
-      const inviteResponse = await axios.post('/api/events/inviteToEvent', {
-        invitations: {
-          eventId: selectedEvent.id,
-          invitees: friendsToInvite,
-        },
-      });
-      // console.log('iR', inviteResponse)
-      getPeopleForEvent();
-      setFriendsToInvite([]);
-    } catch (err) {
-      console.error('CLIENT ERROR: failed to POST event invites', err);
-    }
-  };
-
-  const attendingFriendsItems = friends
-    .filter((friend: any) => participants.includes(friend.id))
-    .map((friend: any, index: number) => {
-      return (
-        <li key={index}>
-          {friend.firstName} {friend.lastName}: attending
-        </li>
-      );
-    });
-
-  const invitedFriendsItems = friends
-    .filter((friend: any) => invitees.includes(friend.id))
-    .map((friend: any, index: number) => {
-      return (
-        <li key={index}>
-          {friend.firstName} {friend.lastName}: invited
-        </li>
-      );
-    });
-
-  const uninvitedFriendsItems = friends
-    .filter(
-      (friend: any) =>
-        !participants.includes(friend.id) && !invitees.includes(friend.id)
-    )
-    .map((friend: any, index: number) => {
-      return (
-        <li key={index}>
-          {friend.firstName} {friend.lastName}: not invited
-          {isUserAttending && (
-            <Form.Check
-              type='checkbox'
-              id='invite-checkbox'
-              label='Check to queue invite'
-              onChange={() => toggleFriendInvite(friend.id)}
-            />
-          )}
-        </li>
-      );
-    });
-
-  return (
-    <Tab eventKey='people' title='People'>
-      <ul>{attendingFriendsItems}</ul>
-      <ul>{invitedFriendsItems}</ul>
-      <ul>{uninvitedFriendsItems}</ul>
-      {isUserAttending && (
-        <Button
-          disabled={friendsToInvite.length === 0}
-          onClick={() => sendFriendInvites()}
-        >
-          Send Invites
-        </Button>
-      )}
-    </Tab>
-  );
-};
 
 const EventBasicModal: React.FC<EventBasicModalProps> = ({
   selectedEvent,
@@ -348,8 +105,10 @@ const EventBasicModal: React.FC<EventBasicModalProps> = ({
   const [friendsToInvite, setFriendsToInvite] = useState([]); // collects friends to invite as group to event
 
   useEffect(() => {
-    getPeopleForEvent();
-  }, []);
+    if (selectedEvent) {
+      getPeopleForEvent();
+    }
+  }, [selectedEvent]);
 
   // ONLY A USER'S FRIENDS WILL POPULATE THESE AREA
   const getPeopleForEvent = async () => {
@@ -357,7 +116,13 @@ const EventBasicModal: React.FC<EventBasicModalProps> = ({
       `/api/events/getPeopleForEvent/${userId}-${selectedEvent.id}`
     );
     const { eventParticipants, eventInvitees } = eventPeopleData.data;
-    // console.log('inside getPeopleForEvent', 'eP', eventParticipants, 'eI', eventInvitees)
+    console.log(
+      'inside getPeopleForEvent',
+      'eP',
+      eventParticipants,
+      'eI',
+      eventInvitees
+    );
     setInvitees(eventInvitees);
     setParticipants(eventParticipants);
   };
@@ -396,7 +161,7 @@ const EventBasicModal: React.FC<EventBasicModalProps> = ({
     .map((friend: any, index: number) => {
       return (
         <li key={index}>
-          {friend.firstName} {friend.lastName}: attending
+          {friend.firstName} {friend.lastName}
         </li>
       );
     });
@@ -406,7 +171,7 @@ const EventBasicModal: React.FC<EventBasicModalProps> = ({
     .map((friend: any, index: number) => {
       return (
         <li key={index}>
-          {friend.firstName} {friend.lastName}: invited
+          {friend.firstName} {friend.lastName}
         </li>
       );
     });
@@ -419,12 +184,13 @@ const EventBasicModal: React.FC<EventBasicModalProps> = ({
     .map((friend: any, index: number) => {
       return (
         <li key={index}>
-          {friend.firstName} {friend.lastName}: not invited
+          {friend.firstName} {friend.lastName}
           {isUserAttending && (
             <Form.Check
+              style={{ float: 'right', paddingRight: '10px' }}
               type='checkbox'
               id='invite-checkbox'
-              label='Check to queue invite'
+              label='Add invite'
               onChange={() => toggleFriendInvite(friend.id)}
             />
           )}
@@ -435,7 +201,7 @@ const EventBasicModal: React.FC<EventBasicModalProps> = ({
   ////////////////////////////////////////
 
   return (
-    <Modal className='event-modal' show={showBasicModal} onHide={handleClose}>
+    <Modal show={showBasicModal} onHide={handleClose}>
       <Modal.Header>
         <Modal.Title>
           {selectedEvent.name} {}
@@ -451,22 +217,29 @@ const EventBasicModal: React.FC<EventBasicModalProps> = ({
                 alignContent: 'center',
               }}
             >
-              <div>
+              <div className='my-2 px-2'>
                 <EventBasicMapComponent
                   latitude={selectedEvent.latitude}
                   longitude={selectedEvent.longitude}
                 />
               </div>
+
               <div>
-                <p>
-                  <b>What: </b>
-                  {selectedEvent.description}
-                </p>
-                <p>
-                  <b>When: </b>
-                  <em>{dayjs().to(dayjs(selectedEvent.startTime))}</em>{' '}
-                  {selectedEvent.startTime} to {selectedEvent.endTime}{' '}
-                </p>
+                <div>
+                    <b>What: </b>
+                    {selectedEvent.description}
+                </div>
+
+                <div className='d-flex flex-column'>
+                  <div>
+                    <b>When: </b>
+                    {dayjs().to(dayjs(selectedEvent.startTime))}
+                  </div>
+                  <div>
+                    <em>{dayjs(selectedEvent.startTime).format('ddd MMM D, h:mm a')} to {dayjs(selectedEvent.endTime).format('h:mm a')}</em>
+                  </div>
+                </div>
+
                 {selectedEvent.address && (
                   <p>
                     <b>Where:</b> {selectedEvent.address}
@@ -477,10 +250,28 @@ const EventBasicModal: React.FC<EventBasicModalProps> = ({
           </Tab>
 
           <Tab eventKey='people' title='People'>
-            <ul>{attendingFriendsItems}</ul>
-            <ul>{invitedFriendsItems}</ul>
-            <ul>{uninvitedFriendsItems}</ul>
-            {isUserAttending && (
+            {attendingFriendsItems.length > 0 && (
+              <div>
+                <h5>Attending</h5>
+                <ul>{attendingFriendsItems}</ul>
+              </div>
+            )}
+
+            {invitedFriendsItems.length > 0 && (
+              <div>
+                <h5>Invited</h5>
+                <ul>{invitedFriendsItems}</ul>
+              </div>
+            )}
+
+            {uninvitedFriendsItems.length > 0 && (
+              <div>
+                <h5>Uninvited</h5>
+                <ul>{uninvitedFriendsItems}</ul>
+              </div>
+            )}
+
+            {isUserAttending && uninvitedFriendsItems.length > 0 && (
               <Button
                 disabled={friendsToInvite.length === 0}
                 onClick={() => sendFriendInvites()}
@@ -489,39 +280,13 @@ const EventBasicModal: React.FC<EventBasicModalProps> = ({
               </Button>
             )}
           </Tab>
-
-          {/* <EventBasicKreweTab
-                isUserAttending={isUserAttending}
-                selectedEvent={selectedEvent}
-                friends={friends}
-                userId={userId}/> */}
-
-          <Tab eventKey='people1' title='People1'>
-            {/* <Form.Switch
-              type='switch'
-              id='attending-switch'
-              label='Attending?'
-              onChange={() => toggleAttendance()}
-              defaultChecked={isUserAttending}
-            /> */}
-
-            {
-              // only allow invites if the user is attending the event
-              <EventBasicAccordion
-                isUserAttending={isUserAttending}
-                selectedEvent={selectedEvent}
-                friends={friends}
-                userId={userId}
-              />
-            }
-          </Tab>
         </Tabs>
       </Modal.Body>
-      <Modal.Footer style={{ display: 'flex', justifyContent: 'around' }}>
+      <Modal.Footer style={{ display: 'flex', justifyContent: 'space-around' }}>
         <Form.Switch
           type='switch'
           id='attending-switch'
-          label='Attending?'
+          label={isUserAttending ? 'Attending' : 'Not attending'}
           onChange={() => toggleAttendance()}
           defaultChecked={isUserAttending}
         />
