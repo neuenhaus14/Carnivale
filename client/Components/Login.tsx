06502bdf8e0a90287259ef7862be192bbb9524e3
gfish94 <grmyfisher@gmail.com>
2024-02-07 13:08:17 -0600

    remove authentication for demo version

--- client/Components/Login.tsx
+++ client/Components/Login.tsx
@@ -1,32 +1,56 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "react-bootstrap";
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from 'react-bootstrap';
+
+import { useNavigate } from 'react-router-dom';
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
+  const navigate = useNavigate();
+
   return (
-    <div id="login">
-      <div style={{
-        border: "1px solid #ccc",
-        borderRadius: "8px",
-        padding: "10px",
-        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-        display: "flex",
-        alignContent: 'center',
-        flexDirection: "column",
-        position: "relative",
-        backgroundColor:"#fffcf8",
-        textAlign: "center",
-        paddingBottom: "2rem",
-      }}>
-      <img id="login-img" src="img/jesterPin.png" alt="jester pin logo" width= "100%" height="auto" />
-      <h3>One stop shop for managing the chaos of Mardi Gras.</h3>
-      <h4>Share gossip, costumes, throws, drop pins with hot commodities, create events with your friends, checkout local music, and lookup parade schedules!</h4><br />
-      <Button className="btn-login" style={{backgroundColor: "#e7abff" }} onClick={() => loginWithRedirect()}>Log In</Button>
+    <div id='login'>
+      <div
+        style={{
+          border: '1px solid #ccc',
+          borderRadius: '8px',
+          padding: '10px',
+          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+          display: 'flex',
+          alignContent: 'center',
+          flexDirection: 'column',
+          position: 'relative',
+          backgroundColor: '#fffcf8',
+          textAlign: 'center',
+          paddingBottom: '2rem',
+        }}
+      >
+        <img
+          id='login-img'
+          src='img/jesterPin.png'
+          alt='jester pin logo'
+          width='100%'
+          height='auto'
+        />
+        <h3>One stop shop for managing the chaos of Mardi Gras.</h3>
+        <h4>
+          Share gossip, costumes, throws, drop pins with hot commodities, create
+          events with your friends, checkout local music, and lookup parade
+          schedules!
+        </h4>
+        <br />
+        <Button
+          className='btn-login'
+          style={{ backgroundColor: '#e7abff' }}
+          // onClick={() => loginWithRedirect()}
+          onClick={() => navigate('/homepage')}
+        >
+          DEMO
+        </Button>
       </div>
     </div>
-  )
+  );
 };
 
 export default LoginButton;
