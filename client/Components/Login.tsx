ce61f206274c483c073e36f77ba836e76f8ea2b7
jshep23prog <jshep23@gmail.com>
2024-02-08 14:57:39 -0600

    plugged in proper survey link

--- client/Components/Login.tsx
+++ client/Components/Login.tsx
@@ -29,7 +29,7 @@ const LoginButton = () => {
 
       <h3>One stop shop for managing the chaos of Mardi Gras.</h3>
       <h4>Share gossip, costumes, throws, drop pins with hot commodities, create events with your friends, checkout local music, and lookup parade schedules!</h4>
-      <p>Take the <a href="https://docs.google.com/forms/d/e/1FAIpQLSdifqUvq5LZ-U9eNAScytOQXriQly0f90yA3W7-JLVIEfgiOQ/viewform?usp=sf_link">Survey</a> and let us know what you think!</p>
+      <p>Take the <a href="https://docs.google.com/forms/d/e/1FAIpQLSfSGLNva3elpadLqpXw1WuD9b4H39lBuX6YMiKT5_o2DNQ7Gg/viewform">Survey</a> and let us know what you think!</p>
       <p>Like what you see?  Support us on <a href="https://www.kickstarter.com">Kickstarter</a></p><br />
       <p className='disclaimer'>Pardigras.org is a beta website and is still in the beginning stages. We DO NOT use your location and user information for monetary gain. By logging in you agree to forfeit litigation against the website or creators. </p>
       <Button className="btn-login" style={{backgroundColor: "#e7abff" }} onClick={() => loginWithRedirect()}>Log In</Button>
