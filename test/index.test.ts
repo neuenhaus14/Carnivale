aa9ff7e4a0eb358ad11ef05e2f33d546c1b93006
evmaperry <evmaperry@gmail.com>
2024-03-04 12:07:03 -0500

    TEST / add dummy test to UserPage tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,27 +6,30 @@ import server from '../server/app';
 
 describe('Pardi Gras', function () {
 
-  before(async function () {
+  before(function () {
     console.log('BEFORE TESTS');
 
-    // spin up server, which connects to test db as per environment variable set in test script
-    server.listen(4001, () => {
-      console.log('Listening at port 4001');
+    // spin up server, which connects to test db as per environment variable set in test script. port is 4000, accounting for return url for Auth0 login.
+    server.listen(4000, () => {
+      console.log('Listening at port 4000');
     });
   });
 
   describe('Array', function () {
     describe('#indexOf()', function () {
       it('should return -1 when the value is not present', function () {
-        assert?.equal([1, 2, 3].indexOf(4), -1);
+        assert.equal([1, 2, 3].indexOf(4), -1);
       });
     });
   });
 
   describe('UserPage', function () {
-    userPageTest();
+    userPageTest()
   })
 
+  after(()=> {
+    server.close(()=>console.log('server closed'))
+  })
 });
 
 export default {};
--- test/userPage.test.ts
+++ test/userPage.test.ts
@@ -2,8 +2,11 @@ import assert from 'assert';
 import { describe, it } from 'mocha';
 
 const userPageTest = () => {
-  console.log('inside userPageTest')
-}
+  describe('UserPage', () => {
+    it('tests equivalence of 1', function () {
+      assert.equal(1, 1);
+    });
+  });
+};
 
 export default userPageTest;
-
